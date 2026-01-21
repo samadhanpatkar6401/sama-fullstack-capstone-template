ec39dff4ac2b1dae77127a61f3bf0973a7709a66
samadhanpatkar6401 <samadhanpatkar4@gmail.com>
2026-01-21 08:02:15 -0500

    Add database connection and gift/search API routes

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -5,52 +5,50 @@ const cors = require('cors');
 const pinoLogger = require('./logger');
 
 const connectToDatabase = require('./models/db');
-const {loadData} = require("./util/import-mongo/index");
-
+const { loadData } = require("./util/import-mongo/index");
 
 const app = express();
-app.use("*",cors());
+app.use("*", cors());
 const port = 3060;
 
 // Connect to MongoDB; we just do this one time
 connectToDatabase().then(() => {
     pinoLogger.info('Connected to DB');
 })
-    .catch((e) => console.error('Failed to connect to DB', e));
-
+.catch((e) => console.error('Failed to connect to DB', e));
 
 app.use(express.json());
 
-// Route files
-// Gift API Task 1: import the giftRoutes and store in a constant called giftroutes
-//{{insert code here}}
+// -------------------- ROUTE FILES --------------------
+// Gift API Task 1: import the giftRoutes
+const giftRoutes = require('./routes/giftRoutes');
 
-// Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
-//{{insert code here}}
+// Search API Task 1: import the searchRoutes
+const searchRoutes = require('./routes/searchRoutes');
 
-
+// -------------------- LOGGER --------------------
 const pinoHttp = require('pino-http');
 const logger = require('./logger');
 
 app.use(pinoHttp({ logger }));
 
-// Use Routes
-// Gift API Task 2: add the giftRoutes to the server by using the app.use() method.
-//{{insert code here}}
+// -------------------- USE ROUTES --------------------
+// Gift API Task 2: add the giftRoutes to the server
+app.use('/api/gifts', giftRoutes);
 
-// Search API Task 2: add the searchRoutes to the server by using the app.use() method.
-//{{insert code here}}
+// Search API Task 2: add the searchRoutes to the server
+app.use('/api/search', searchRoutes);
 
-
-// Global Error Handler
+// -------------------- GLOBAL ERROR HANDLER --------------------
 app.use((err, req, res, next) => {
     console.error(err);
     res.status(500).send('Internal Server Error');
 });
 
-app.get("/",(req,res)=>{
-    res.send("Inside the server")
-})
+// Simple test route
+app.get("/", (req, res) => {
+    res.send("Inside the server");
+});
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
